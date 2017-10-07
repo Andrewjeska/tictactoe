9a5400f2ecfe920f9e0a6c5b46461af08941b43b
Michael <m.anderjaska@gmail.com>
2017-10-07 15:01:16 -0400

    added name

---
+++ gomoku/src/Player/Sashael_Thupujaska.hs
@@ -0,0 +1,10 @@
+Player.Sashael_Thupujaska (playerSashael) where
+
+import Types
+
+teamMembers :: String 
+teamMembers = "Michael Anderjaska and Sashank Thupukari"
+
+playerSashael :: Player 
+playerSashael = error "Define me!"
+
