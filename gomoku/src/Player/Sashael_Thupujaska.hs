07e50fc89dae0ab20c8ab7fde030662364f87265
Michael <m.anderjaska@gmail.com>
2017-10-07 22:06:44 -0600

    this might work?

--- gomoku/src/Player/Sashael_Thupujaska.hs
+++ gomoku/src/Player/Sashael_Thupujaska.hs
@@ -2,12 +2,14 @@ module Player.Sashael_Thupujaska (playerSashael_Thupujaska) where
 
 import Types
 
-teamMembers :: String 
+teamMembers :: String
 teamMembers = "Michael Anderjaska and Sashank Thupukari"
 
-playerSashael_Thupujaska :: Player 
+playerSashael_Thupujaska :: Player
 playerSashael_Thupujaska = Player getNextMove "Sashael_Thupujaska"
 
-getNextMove :: Tile -> Board -> IO (Int, Int)
-getNextMove t b = error "just get the next space"
-
+getNextMove :: Tile -> Board -> IO Move
+getNextMove t b =
+	case b of
+		((x,y), EmptyTile):t -> (x,y)
+		h:t -> getNextMove t t
