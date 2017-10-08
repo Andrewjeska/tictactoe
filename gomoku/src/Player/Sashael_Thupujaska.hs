f7abc8940c4f286a7f17daec2f658531014c6c22
Michael <m.anderjaska@gmail.com>
2017-10-07 22:00:18 -0600

    compiles

--- gomoku/src/Player/Sashael_Thupujaska.hs
+++ gomoku/src/Player/Sashael_Thupujaska.hs
@@ -1,10 +1,13 @@
-Player.Sashael_Thupujaska (playerSashael) where
+module Player.Sashael_Thupujaska (playerSashael_Thupujaska) where
 
 import Types
 
 teamMembers :: String 
 teamMembers = "Michael Anderjaska and Sashank Thupukari"
 
-playerSashael :: Player 
-playerSashael = error "Define me!"
+playerSashael_Thupujaska :: Player 
+playerSashael_Thupujaska = Player getNextMove "Sashael_Thupujaska"
+
+getNextMove :: Tile -> Board -> IO (Int, Int)
+getNextMove t b = error "just get the next space"
 
